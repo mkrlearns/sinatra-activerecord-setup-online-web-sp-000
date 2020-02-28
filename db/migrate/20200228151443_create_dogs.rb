9cbf4ea6ac9b53ed145821e311ec9323e7eab042
ryan m <contact@ryanmeek.com>
2020-02-28 15:18:35 +0000

    Automatically backed up by Learn

--- db/migrate/20200228151443_create_dogs.rb
+++ db/migrate/20200228151443_create_dogs.rb
@@ -1,4 +1,4 @@
 class CreateDogs < ActiveRecord::Migration[5.2]
   def up() create_table(:dogs) { |t| t.string :name; t.string :breed }
-  def down() {drop_table :dogs}
+  def down() drop_table :dogs end
 end
